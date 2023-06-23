e3759d16549fbc0c6945ad7d5a8cf04bacecaf3b
Ahukko8 <ahukko88@gmail.com>
2023-06-23 15:36:34 +0500

    creating modal

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+let myModal = document.getElementById('myModal')
+let myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
+
 const reveals = document.querySelectorAll(".reveal");
 function reveal() {
     for (let i = 0; i < reveals.length; i++) {
@@ -13,3 +43,5 @@ function reveal() {
     }
 }
 window.addEventListener("scroll", reveal);
+
+
